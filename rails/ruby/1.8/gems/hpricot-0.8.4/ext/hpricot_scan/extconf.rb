6a04c9e89ddbf70673ca48f584a431e8cc446e1b
Sacha Chua <sacha@sachachua.com>
2011-11-06 14:59:24 -0500

    Integration tests, stuff tracking

--- rails/ruby/1.8/gems/hpricot-0.8.4/ext/hpricot_scan/extconf.rb
+++
@@ -1,9 +0,0 @@
-require 'mkmf'
-
-$CFLAGS << " #{ENV["CFLAGS"]}"
-CONFIG['optflags'] = '' if $CFLAGS =~ /DEBUG/
-
-dir_config("hpricot_scan")
-have_library("c", "main")
-
-create_makefile("hpricot_scan")
