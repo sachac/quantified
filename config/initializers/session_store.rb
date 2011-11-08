3aa1e8c6ee33b1a9c73c749af930ad2d81004e33
Sacha Chua <sacha@sachachua.com>
2011-11-07 21:17:26 -0500

    Yay fixed login cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Home::Application.config.session_store :cookie_store, :key => '_home_session'
+Home::Application.config.session_store :cookie_store, :key => 'quantifiedawesome', :domain => '.dev.quantifiedawesome.com'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
