750f7f8449532f5eb16e8b3c92a76a4f28b40a6d
Sacha Chua <sacha@sachachua.com>
2011-11-08 22:13:17 -0500

    Clock view

--- app/models/time_record.rb
+++ app/models/time_record.rb
@@ -6,5 +6,17 @@ class TimeRecord < ActiveRecord::Base
   def ends_at
     @end_time
   end
-
+  def category
+    if name.match /^A - Work/ 
+      'Work'
+    elsif name.match /^A - Sleep/ 
+      'Sleep'
+    elsif name.match /^D - / 
+      'Discretionary'
+    elsif name.match /^UW - / 
+      'Unpaid work'
+    elsif name.match /^P - / 
+      'Personal care'
+    end
+  end
 end
