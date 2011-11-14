89f5e9f107101ee3789bdff1b38b6c8abdf73daa
Sacha Chua <sacha@sachachua.com>
2011-11-13 20:14:37 -0500

    Add contexts for stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,9 +31,9 @@ class ApplicationController < ActionController::Base
   
   def current_account
     if (current_subdomain.nil?)
-      @account = User.first
+      current_user || User.first
     else
-      @account ||= User.find_by_username(current_subdomain)  
+      User.find_by_username(current_subdomain)  
     end
   end  
   # Make this method visible to views as well  
