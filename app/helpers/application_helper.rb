609f080bdad14304f2445985e86de27255fde235
Sacha Chua <sacha@sachachua.com>
2011-11-17 08:01:53 -0500

    New style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -93,4 +93,16 @@ module ApplicationHelper
     location_name = location.is_a?(String) ? location : location.name
     link_to location_name, log_stuff_path(:stuff_name => stuff.name, :location_name => location_name, :destination => destination), :method => :post
   end
+
+  def conditional_html( lang = "en", &block )
+    haml_concat Haml::Util::html_safe <<-"HTML".gsub( /^\s+/, '' )
+    <!--[if lt IE 7 ]>              <html lang="#{lang}" class="no-js ie6"> <![endif]-->
+    <!--[if IE 7 ]>                 <html lang="#{lang}" class="no-js ie7"> <![endif]-->
+    <!--[if IE 8 ]>                 <html lang="#{lang}" class="no-js ie8"> <![endif]-->
+    <!--[if IE 9 ]>                 <html lang="#{lang}" class="no-js ie9"> <![endif]-->
+    <!--[if (gte IE 9)|!(IE)]><!--> <html lang="#{lang}" class="no-js"> <!--<![endif]-->      
+  HTML
+    haml_concat capture( &block ) << Haml::Util::html_safe( "\n</html>" ) if block_given?
+  end
+
 end
