c1e8e9669a93821a70115d537f2a36be64adebbc
Sacha Chua <sacha@sachachua.com>
2011-12-06 20:41:43 -0500

    Add more navigability

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-        $('.datepicker').datepicker({dateFormat: 'yy-m-d'});
-				$('.sparkline-bar').sparkline('html', {type: 'bar', barColor: '#85acaa', chartRangeMin: 0});
-
-    });
+		$('.datepicker').datepicker({dateFormat: 'yy-m-d'});
+		$('.sparkline-bar').sparkline('html', {type: 'bar', barColor: '#85acaa', chartRangeMin: 0});
+		$('.collapsed .details').hide();
+		$('.collapsible legend').click(function() { $(this).next('.details').slideToggle(500); });		
+	});
