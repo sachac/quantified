61ede776debc1c02ebba2aed0484870a245d7884
Sacha Chua <sacha@sachachua.com>
2011-12-09 14:51:57 -0500

    Add blog processor

--- spec/views/tap_log_records/index.html.haml_spec.rb
+++ spec/views/tap_log_records/index.html.haml_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe "tap_log_records/index.html.haml" do
   before(:each) do
     as_user
-    assign(:tap_log_records, [
-                              Factory(:tap_log_record),
-                              Factory(:tap_log_record)
-    ])
+    Factory(:tap_log_record)
+    Factory(:tap_log_record)
   end
 
   it "renders a list of tap_log_records" do
+    assign(:tap_log_records, TapLogRecord.paginate(:page => 1))
+
     render
   end
 end
