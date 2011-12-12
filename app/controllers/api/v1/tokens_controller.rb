3c952938e3c5b4da89daf0631f7dedcaf5eb3c86
Sacha Chua <sacha@sachachua.com>
2011-12-11 20:10:25 -0500

    Tokens, Tap Log record

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::TokensController  < ApplicationController      
+  skip_before_filter :verify_authenticity_token      
+  skip_authorization_check :only => [:create, :destroy]
+  respond_to :json, :xml
+  def create          
+    login = params[:login]          
+    password = params[:password]               
+    if login.nil? or password.nil?
+      status = 400
+      message = {:message => "The request must contain the user login and password."}
+    else
+      @user = User.find_record(login)
+      if @user
+        @user.ensure_authentication_token!
+      end
+      if @user.nil? || !@user.valid_password?(password)
+        logger.info("User #{login} failed signin, user cannot be found.")
+        status = 400; message = {:message => "Invalid login or password."}
+      else
+        status = 200; message = {:token => @user.authentication_token}
+      end
+    end
+    respond_to do |format|
+      format.json { render(:status => status, :json => message) and return }
+      format.xml { render(:status => status, :xml => message) and return }
+    end
+    return
+  end
+
+  def destroy
+    @user = User.find_by_authentication_token(params[:token])
+    if @user.nil?
+      logger.info("Token not found")
+      status = 400; message = "Invalid token"
+    else
+      @user.reset_authentication_token!
+      status = 200; message = {:token => params[:token]}
+    end
+    respond_to do |format|
+      format.json { render(:status => status, :json => {:message=> message}) and return }
+      format.xml { render(:status => status, :xml => {:message=> message}) and return }
+    end
+    return
+  end  
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
   attr_accessor :login
 
   # Setup accessible (or protected) attributes for your model
