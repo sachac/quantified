514331db82159b6e17ba5910a40f9657b91dca63
Sacha Chua <sacha@sachachua.com>
2011-12-31 20:21:16 -0500

    Offline data entry

--- app/controllers/api/offline/v1/offline_controller.rb
+++ app/controllers/api/offline/v1/offline_controller.rb
@@ -14,14 +14,44 @@ class Api::Offline::V1::OfflineController < ApplicationController
       end
     end
     authorize! :manage_account, current_account
-    if params[:record_category_id] and params[:date] and current_account
-      cat = current_account.record_categories.find_by_id(params[:record_category_id])
-      if cat
-        @record = Record.create(:user => current_account, :timestamp => (params[:date] =~ /^[0-9]+/) ? Time.at((params[:date].to_i / 1000).to_i).in_time_zone : Time.zone.parse(params[:date]), :record_category => cat, :source => 'offline')
-        @record.update_previous
-        respond_with @record and return
-      else
-        render :text => "Could not find category", :status => 404 and return
+    if params[:type] == 'track' || params[:type].nil?
+      if params[:record_category_id] and params[:date] and current_account
+        cat = current_account.record_categories.find_by_id(params[:record_category_id])
+        if cat
+          timestamp = (params[:date] =~ /^[0-9]+/) ? Time.at((params[:date].to_i / 1000).to_i).in_time_zone : Time.zone.parse(params[:date]);
+          @record = current_account.records.find_by_timestamp(timestamp)
+          unless @record
+            @record = Record.create(:user => current_account, :timestamp => timestamp, :record_category => cat, :source => 'offline')
+            data = cat.data
+            @record.data ||= Hash.new
+            if params[:data]
+              params[:data].each do |k, hash|
+                if data[hash['name'].to_sym]
+                  @record.data[hash['name']] = hash['value']
+                end
+              end
+            end
+            @record.save!
+            @record.update_previous
+          end
+          respond_with @record and return
+        else
+          render :text => "Could not find category", :status => 404 and return
+        end
+      end
+    elsif params[:type] == 'edit'
+      if params[:id] and current_account
+        record = current_account.records.find_by_id(params[:id])
+        data = record.record_category.data
+        logger.info params[:data]
+        record.data ||= Hash.new
+        params[:data].each do |k, hash|
+          if data[hash['name'].to_sym]
+            record.data[hash['name']] = hash['value']
+          end
+        end
+        record.save!
+        respond_with record and return
       end
     end
     render :text => "Error", :status => 500 and return
