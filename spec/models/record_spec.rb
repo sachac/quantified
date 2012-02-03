9b4011764c29b2bdabf0af63dc4a5dfd46f73ac3
Sacha Chua <sacha@sachachua.com>
2012-02-02 19:17:50 -0500

    Preserve timestamp when disambiguating category (fixes #387)

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Record do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#guess_time" do
+    it "leaves no-time entries alone" do
+      o = Record.guess_time("hello")
+      o[0].should == "hello"
+      o[1].should be_nil
+    end
+    it "deals with nil" do
+      o = Record.guess_time(nil)
+      o[0].should be_nil
+      o[1].should be_nil
+    end
+    it "recognizes hh:mm string" do
+      o = Record.guess_time("8:00 test")
+      o[0].should == "test"
+      o[1].hour.should == 8
+    end
+    it "recognizes string hh:mm" do
+      o = Record.guess_time("test 8:00")
+      o[0].should == "test"
+      o[1].hour.should == 8
+    end
+  end
 end
