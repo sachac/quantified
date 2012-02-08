fd5777f06458973b36d576f91b683ec1461eb1bb
Sacha Chua <sacha@sachachua.com>
2012-02-07 20:36:31 -0500

    Schedule

--- app/assets/javascripts/quantified.js
+++ app/assets/javascripts/quantified.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		$('.sparkline-bar').sparkline('html', {type: 'bar', barColor: '#85acaa', chartRangeMin: 0});
 		$('.sparkline').sparkline('html', {enableTagOptions: true, type: 'bullet'});
 		$('.collapsed .details').hide();
-		//		$('.collapsible legend').unbind('click').click(function() { $(this).next('.details').slideToggle(500); });
+		$('.collapsible legend').unbind('click').click(function() { $(this).next('.details').slideToggle(500); });
 		jQuery('time.timeago').timeago();
 		$('.tooltip-fixed').qtip({fixed: true, hide: { delay: 1000, event: 'mouseout' }, content: { text: 'Loading...', ajax: { url: $(this).attr('rel') }}});
 	});
