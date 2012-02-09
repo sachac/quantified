c7ed9214c1159e0dec1a68a2a1b97590554d23b9
Sacha Chua <sacha@sachachua.com>
2012-02-08 22:14:30 -0500

    Be more careful about updating timestamps! Only update matching

--- app/models/record_category.rb
+++ app/models/record_category.rb
@@ -10,9 +10,14 @@ class RecordCategory < ActiveRecord::Base
     self.full_name = self_and_ancestors.reverse.map{ |c| c.name }.join(' - ').html_safe
   end
 
+  # :tree =>
+  #    :full - parent totals include that of child categories, show all categories below this
+  #    :next_level - only the next level of children is shown
+  #    other - each entry counts only towards its own category
   def self.roll_up_records(options = {})
     summary = Hash.new { |h,k| h[k] = Hash.new { |h2,k2| h2[k2] = Hash.new { |h3,k3| h3[k3] = 0 } } }
     records = options[:records]
+    user = options[:user]
     max = Time.now
     zoom = options[:zoom]
     if options[:range]
@@ -20,22 +25,37 @@ class RecordCategory < ActiveRecord::Base
       max = options[:range].end.midnight.in_time_zone
       min = options[:range].begin.midnight.in_time_zone
     end
+    records ||= user.records
     records = records.activities
+    categories = user.record_categories.index_by(&:id)
+    # Cache the record categories for performance instead of retrieving them one at a time
     records.each do |rec|
-      split = rec.split(options[:range])
-      case options[:tree]
-      when :full
-        categories = rec.record_category.self_and_ancestors
-      when :next_level
-        categories = [rec.record_category.as_child(options[:parent])].compact
-      else
-        categories = [rec.record_category]
+      ids = nil
+      if categories[rec.record_category_id]
+        case options[:tree]
+        when :full
+          if categories[rec.record_category_id].dotted_ids
+            ids = categories[rec.record_category_id].dotted_ids.split('.')
+          end
+        when :next_level
+          ids = [rec.record_category.as_child(options[:parent].dotted_ids)].compact
+        else
+          ids = [rec.record_category_id]
+        end
       end
-      if categories.length > 0
-        split.each do |split_record|
+      if ids.length > 0
+        rec.split(options[:range]).each do |split_record|
           key = Record.get_zoom_key(options[:user], zoom, split_record[0])
-          categories.each do |cat|
-            summary[:rows][cat][key] += split_record[1] - split_record[0]
+          if key == Date.parse('2012-01-10')
+            puts "#{split_record[2].id} #{key} #{ids.inspect}\t#{split_record[0]}\t#{split_record[1]}\t#{split_record[1] - split_record[0]}"
+          end
+          ids.each do |cat|
+            case options[:key]
+            when :date
+              summary[:rows][key][categories[cat]] += split_record[1] - split_record[0]
+            else
+              summary[:rows][categories[cat]][key] += split_record[1] - split_record[0]
+            end
           end
           summary[:total][:total][key] += split_record[1] - split_record[0]
         end
@@ -43,7 +63,7 @@ class RecordCategory < ActiveRecord::Base
     end
     summary
   end
-
+    
   def self.find_or_create(user, path)
     count = path.length
     parent = nil
@@ -66,7 +86,7 @@ class RecordCategory < ActiveRecord::Base
     end
     cat
   end
-
+  
   def self.summarize(options = {})
     options[:records] ||= options[:user].records
     options[:range] ||= records.min('timestamp')..records.max('timestamp')
@@ -100,6 +120,22 @@ class RecordCategory < ActiveRecord::Base
     Record.joins(:record_category).where('dotted_ids LIKE ?', self.dotted_ids + '.%')
   end
 
+  # Given: 1.2.3, 1.2.3.4.5, return 4 (the next-level child of parent)
+  # Given: 1.2.3, 4, return nil (item is not a child of parent)
+  def self.as_child_id(parent, child)
+    if child[0, parent.length + 1] == parent + "."
+      child[parent.length + 1, child.length - parent.length].split('.')[0]
+    end
+  end
+  def as_child_id(parent)
+    # If this category is a descendant of parent, return the next level in the category tree below parent
+    id = Record.as_child_id(parent, self.dotted_ids)
+    if parent.is_a? RecordCategory
+      return RecordCategory.find_by_id(id) if id 
+    else
+      return id
+    end
+  end
   # Returns this category as a child of parent
   def as_child(parent)
     x = self
@@ -111,7 +147,7 @@ class RecordCategory < ActiveRecord::Base
       x
     end
   end
-
+  
   def get_color
     x = self
     while x != nil
