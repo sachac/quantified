64048ea30f696f9f330746f5befee20fd55d09b3
Sacha Chua <sacha@sachachua.com>
2012-03-01 16:15:31 -0500

    Add the ability to add to an existing CSA log

--- app/models/csa_food.rb
+++ app/models/csa_food.rb
@@ -14,4 +14,18 @@ class CsaFood < ActiveRecord::Base
     # 6 - 5 -   2
     date + (7 - (date.wday + 3) % 7).days
   end
+
+  def self.log(account, options)
+    if options[:food].is_a? Food
+      food = options[:food]
+    else
+      food = Food.get_food(account, options[:food])
+    end
+    past = account.csa_foods.where(:food_id => food.id, :date_received => options[:date_received] || Date.today).first
+    if past
+      past.update_attributes(:quantity => past.quantity + options[:quantity])
+    else
+      account.csa_foods.create(:quantity => options[:quantity], :food => food, :unit => options[:unit], :date_received => options[:date_received])
+    end
+  end
 end
