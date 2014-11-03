feab3582a4f72dbbe3a0c884b844a89cb69b5c93
Sacha Chua <sacha@sachachua.com>
2014-11-03 13:10:32 -0500

    Fix WIP goal test

--- features/goals.feature
+++ features/goals.feature
@@ -5,12 +5,12 @@ Feature: Goals
   Background:
     Given I am logged in
     And I have a record category named "Work on awesome things"
-  @wip
   Scenario: Define a greater-than-or-equal-to goal for a category
     When I view the record category named "Work on awesome things"
     And I define a goal for it
     And I name the goal "Work on awesome things weekly"
-    And I set the goal to be greater than or equal to 1 hour a week
+    And I set the goal to be >= 1 hour
+    And I set the goal to be weekly
     And I save it
     And I view my dashboard
     Then I should see "Work on awesome things weekly"
--- features/step_definitions/goals_steps.rb
+++ features/step_definitions/goals_steps.rb
@@ -7,11 +7,22 @@ When(/^I name the goal "(.*?)"$/) do |arg1|
   fill_in 'goal[label]', with: arg1
 end
 
-When(/^I set the goal to be (>=) to ([\.0-9]+) hours?$/) do |op, arg1|
-  fill_in 'goal[hours]', with: arg1
-  select(op, from: 'goal[op]')
+When(/^I set the goal to be (>=) (to )?([\.0-9]+) hours?$/) do |op, ignore, arg1|
+  choose 'expression_type_direct'
+  select(op, from: 'direct_op')
+  fill_in 'direct_target', with: arg1
 end
 
 When(/^I set the goal to be (daily|weekly|today|monthly)$/) do |arg1|
-  select(arg1, from: 'goal[period]')
+  value = case arg1
+         when 'daily'
+           'Past 24 hours'
+         when 'weekly'
+           'This week'
+         when 'today'
+           'Today'
+         when 'monthly'
+           'This month'
+         end
+  select(value, from: 'goal[period]')
 end
