96b387697955c32ea60ac3793c4dbca47e387c0d
Sacha Chua <sacha@sachachua.com>
2015-02-03 14:27:28 +0000

    Move coveralls code higher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
+require 'coveralls'
+Coveralls.wear_merged!
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'simplecov'
-require 'coveralls'
-Coveralls.wear_merged!
 ActiveRecord::Migration.maintain_test_schema!
 SimpleCov.start 'rails' do
   use_merging true
