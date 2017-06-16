6aca82b4a0e3212e621716ea744eb10e51028afd
Sacha Chua <sacha@sachachua.com>
2017-06-16 00:55:04 -0400

    Silence routing errors

---
+++ config/initializers/logger.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+    class ActionDispatch::DebugExceptions
+      alias_method :old_log_error, :log_error
+      def log_error(env, wrapper)
+        if wrapper.exception.is_a?  ActionController::RoutingError
+          return
+        else
+          old_log_error env, wrapper
+        end
+      end
+    end
+end
