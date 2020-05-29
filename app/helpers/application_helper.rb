d04d9cfb577264c18d2a88b9077b29622037a807
Sacha Chua <sacha@sachachua.com>
2020-05-28 22:27:44 -0400

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,17 +12,7 @@ module ApplicationHelper
     javascript_tag("$('#{id}').focus()");
   end
   def google_analytics_js
-    javascript_tag('
-  var _gaq = _gaq || [];
-  _gaq.push(["_setAccount", "UA-27780597-1"]);
-  _gaq.push(["_setDomainName", "quantifiedawesome.com"]);
-  _gaq.push(["_trackPageview"]);
-
-    (function() {
-    var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
-    ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-    var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
-     })();')
+    ""
   end	
   def clothing_thumbnail(clothing, options = {})
     return unless clothing
