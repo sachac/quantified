02354589fb3d6d8eca861c67f8de56dc8d8121ac
Sacha Chua <sacha@sachachua.com>
2025-04-25 13:30:15 -0400

    add download SVG

--- app/assets/javascripts/quantified.js
+++ app/assets/javascripts/quantified.js
@@ -1,3 +1,20 @@
+function addDownloadSVGButtons() {
+	document.querySelectorAll('svg').forEach((o) => {
+		const button = document.createElement('button');
+		button.textContent = 'Download SVG';
+		button.classList.add('download-svg');
+		o.closest('div').after(button);
+		button.addEventListener('click', function(event) {
+			const element = document.createElement('a');
+			const blob = new Blob([o.outerHTML.toString()]);
+			element.download = 'time.svg';
+			element.href = window.URL.createObjectURL(blob);
+			document.body.appendChild(element);
+			element.click();
+			element.remove();
+		});
+	});
+}
 $(document).ready(function() {
 		$('.datepicker').datepicker({dateFormat: 'yy-m-d'});
 		$('.sparkline-bar').sparkline('html', {type: 'bar', barColor: '#85acaa', chartRangeMin: 0});
@@ -6,5 +23,6 @@ $(document).ready(function() {
 		$('.collapsed .details').hide();
 		$('.collapsible legend').unbind('click').click(function() { $(this).next('.details').slideToggle(500); });
 		jQuery('time.timeago').timeago();
-		$('.tooltip-fixed').qtip({fixed: true, hide: { delay: 1000, event: 'mouseout' }, content: { text: 'Loading...', ajax: { url: $(this).attr('rel') }}});
-	});
+	$('.tooltip-fixed').qtip({fixed: true, hide: { delay: 1000, event: 'mouseout' }, content: { text: 'Loading...', ajax: { url: $(this).attr('rel') }}});
+	addDownloadSVGButtons();
+});
